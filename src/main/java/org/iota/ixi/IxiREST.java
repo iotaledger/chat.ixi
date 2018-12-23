c1df486c7cf39563b05e2bea84713c6c316d3f8d
Samuel Rufinatscha <samuel.rufinatscha@gmail.com>
2018-12-23 16:52:25 +0100

    REST-Service Example

---
+++ src/main/java/org/iota/ixi/IxiREST.java
@@ -0,0 +1,56 @@
+package org.iota.ixi;
+
+import org.iota.ict.ixi.IxiModule;
+import org.iota.ict.model.Transaction;
+import org.iota.ict.network.event.GossipFilter;
+import org.iota.ict.network.event.GossipReceiveEvent;
+import org.iota.ict.network.event.GossipSubmitEvent;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static spark.Spark.get;
+
+
+public class IxiREST extends IxiModule implements Runnable {
+
+    private BlockingQueue<Transaction> toPrint = new LinkedBlockingQueue<>();
+
+    public static final String ADDRESS = "IXI9CHAT9999999999999999999999999999999999999999999999999999999999999999999999999";
+    public static final String NAME = "chat.ixi";
+
+    public IxiREST() {
+        super(NAME);
+    }
+
+    @Override
+    public void onIctConnect(String name) {
+        setGossipFilter(new GossipFilter().watchAddress(ADDRESS));
+        System.out.println("Connected!");
+        new Thread(this).start();
+    }
+
+    @Override
+    public void onTransactionReceived(GossipReceiveEvent event) {
+        toPrint.add(event.getTransaction());
+        System.out.println("RECEIVED");
+    }
+
+    @Override
+    public void onTransactionSubmitted(GossipSubmitEvent event) { ; }
+
+    @Override
+    public void run() {
+
+        get("/getMessage/", (request, response) -> {
+           Transaction t = toPrint.take();
+           return "[" + t.hash + "] " + t.decodedSignatureFragments;
+        });
+
+    }
+
+    public static void main(String[] args) {
+        new IxiREST();
+    }
+
+}
