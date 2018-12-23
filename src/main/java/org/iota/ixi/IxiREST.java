39b6a2669a5838b4c9597a6e5dc7cfa9e725deb4
Samuel <samuel.rufinatscha@gmail.com>
2018-12-23 19:53:06 +0100

    spark add cors-access-control-allow-origin

--- src/main/java/org/iota/ixi/IxiREST.java
+++ src/main/java/org/iota/ixi/IxiREST.java
@@ -7,6 +7,10 @@ import org.iota.ict.network.event.GossipFilter;
 import org.iota.ict.network.event.GossipReceiveEvent;
 import org.iota.ict.network.event.GossipSubmitEvent;
 import org.json.JSONObject;
+import spark.Filter;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -38,6 +42,7 @@ public class IxiREST extends IxiModule {
     public void onIctConnect(String name) {
 
         setGossipFilter(gossipFilter);
+        CorsFilter.apply();
 
         get("/addChannel/:channel", (request, response) -> {
             gossipFilter.getWatchedAddresses().add(request.params(":channel"));
@@ -83,5 +88,29 @@ public class IxiREST extends IxiModule {
 
     @Override
     public void onTransactionSubmitted(GossipSubmitEvent event) { ; }
+
+    public static class CorsFilter {
+
+        private final static HashMap<String, String> corsHeaders = new HashMap<>();
+
+        public CorsFilter() {
+            corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+            corsHeaders.put("Access-Control-Allow-Origin", "*");
+            corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
+            corsHeaders.put("Access-Control-Allow-Credentials", "true");
+        }
+
+        public static void apply() {
+            Filter filter = new Filter() {
+                @Override
+                public void handle(Request request, Response response) throws Exception {
+                    corsHeaders.forEach((key, value) -> {
+                        response.header(key, value);
+                    });
+                }
+            };
+            Spark.after(filter);
+        }
+    }
 
 }
