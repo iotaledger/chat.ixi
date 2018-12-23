7b19fd710ebdbd01656ce57d616c0057c496c498
Samuel Rufinatscha <samuel.rufinatscha@gmail.com>
2018-12-23 16:53:15 +0100

    Add files via upload

---
+++ src/main/java/org/iota/ixi/Main.java
@@ -0,0 +1,39 @@
+package org.iota.ixi;
+
+import org.iota.ict.Ict;
+import org.iota.ict.model.TransactionBuilder;
+import org.iota.ict.utils.Constants;
+import org.iota.ict.utils.Properties;
+
+/**
+ * This class is just for testing your IXI, so you don't have to run Ict manually.
+ * */
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        new IxiREST();
+
+        Properties p1 = new Properties();
+        p1.port = 1341;
+        p1.ixiEnabled = true;
+        p1.ixis.add("chat.ixi");
+
+        Properties p2 = new Properties();
+        p2.port = 1342;
+
+        Ict ict1 = new Ict(p1);
+        Ict ict2 = new Ict(p2);
+
+        ict1.neighbor(ict2.getAddress());
+        ict2.neighbor(ict1.getAddress());
+
+        String name = "Samuel";
+        String message = "hallo test";
+        TransactionBuilder t = new TransactionBuilder();
+        t.address = "IXI9CHAT9999999999999999999999999999999999999999999999999999999999999999999999999";
+        t.asciiMessage(name + ": " + message);
+        ict2.submit(t.build());
+
+    }
+}
