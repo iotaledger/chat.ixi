07d8c596d8a293d612d3b98582a168048dd1d23b
Samuel <samuel.rufinatscha@gmail.com>
2018-12-26 02:09:32 +0100

    Uploading sweetalert license.

--- web/main.js
+++ web/main.js
@@ -1,4 +1,4 @@
-const REST_URL = "http://localhost:4567/";
+const REST_URL = "http://188.68.46.251:4567/";
 const REST_URL_GET = REST_URL+"getMessage/";
 const REST_URL_SUBMIT = REST_URL+"submitMessage/";
 const REST_URL_ADD_CHANNEL = REST_URL+"addChannel/";
